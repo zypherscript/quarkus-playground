126ae71810b07e3e6f74cdc3735e0a5f6b6c4034
zypherscript <kitti.kthp@gmail.com>
2023-04-19 03:48:40 +0700

    more test for endpoints

--- src/test/java/com/example/demo/HelloResourceTest.java
+++ src/test/java/com/example/demo/HelloResourceTest.java
@@ -1,21 +1,40 @@
 package com.example.demo;
 
-import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
 
 @QuarkusTest
 public class HelloResourceTest {
 
-    @Test
-    public void testHelloEndpoint() {
-        given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("Hello from RESTEasy Reactive"));
-    }
+  @Test
+  public void testHelloEndpoint() {
+    given()
+        .when().get("/hello")
+        .then()
+        .statusCode(200)
+        .body(is("Hello from RESTEasy Reactive"));
+  }
+
+  @Test
+  public void testPoliteHelloEndpoint() {
+    given()
+        .when().get("hello/polite/hello/{name}", "G")
+        .then()
+        .statusCode(200)
+        .body(is("Hello Mr/Mrs G"));
+  }
+
+  @Test
+  public void testPoliteGreetingEndpoint() {
+    given()
+        .pathParam("name", "G")
+        .when().get("/hello/polite/greeting/{name}")
+        .then()
+        .statusCode(200)
+        .body(is("Good morning G"));
+  }
 
 }
